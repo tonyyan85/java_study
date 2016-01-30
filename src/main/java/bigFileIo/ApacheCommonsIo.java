faacfbd8564b6ce2f9d81b9ee9b05db38b52a808
yan <computeryd@gmail.com>
2016-01-30 09:37:28 +0800

    提交代码

--- src/main/java/bigFileIo/ApacheCommonsIo.java
+++ src/main/java/bigFileIo/ApacheCommonsIo.java
@@ -4,7 +4,10 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * 我们只需要遍历文件的每一行，然后做相应的处理，处理完之后把它扔掉。
@@ -15,18 +18,60 @@ import java.io.IOException;
 public class ApacheCommonsIo {
 
     public static void main(String args[]) throws IOException {
-        String file = "D:\\google.py";
-        LineIterator it = FileUtils.lineIterator(new File(file), "UTF-8");
-        try {
-            while (it.hasNext()) {
-                String line = it.nextLine();
-                System.out.println(line);
-                // do something with line
+        String filePath = "D://google.py";
+        File file = new File(filePath);
+        int lenght  = 10;
+        LineIterator it = FileUtils.lineIterator(file, "UTF-8");
+        for(int i =1;it.hasNext();i++){
+            //分割
+            //每个块建立一个输出
+            FileWriter output = new FileWriter("D://" + i + ".txt");
+            try {
+                for (long lineCounter = 0; lineCounter < lenght
+                        && it.hasNext(); ++lineCounter) {
+                    String line = it.nextLine();
+                    System.out.println(line);
+                    output.append(line+"\r");
+                    // do something with line
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                output.flush();
+                output.close();
+                output = null;
+
             }
-        } catch (Exception e) {
+        }
+        LineIterator.closeQuietly(it);
+    }
+
+
+//    使用scanner
+    public void tryScanner(String path) throws IOException {
+        FileInputStream inputStream = null;
+        Scanner sc = null;
+        try {
+            inputStream = new FileInputStream(path);
+            sc = new Scanner(inputStream, "UTF-8");
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                // System.out.println(line);
+            }
+            // note that Scanner suppresses exceptions
+            if (sc.ioException() != null) {
+                throw sc.ioException();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            LineIterator.closeQuietly(it);
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (sc != null) {
+                sc.close();
+            }
         }
     }
 
--- src/main/java/bigFileIo/FileSplit.java
+++ src/main/java/bigFileIo/FileSplit.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
+ * 这个方法不好用
  * 将一个行数为fileLines的文本文件平均分为splitNum个小文本文件，
  * 其中换行符'r'是linux上的，windows的java换行符是'\r\n'
  */
@@ -21,20 +22,20 @@ public class FileSplit
         int bufferSize = 20 * 1024 * 1024;//设读取文件的缓存为20MB
 
         //建立缓冲文本输入流
-        File file = new File("/media/Data/毕业设计/kdd cup/数据/userid_profile.txt");
+        File file = new File("D://npMyEditBox.log");
         FileInputStream fileInputStream = new FileInputStream(file);
         BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
         InputStreamReader inputStreamReader = new InputStreamReader(bufferedInputStream);
         BufferedReader input = new BufferedReader(inputStreamReader, bufferSize);
 
-        int splitNum = 112-1;//要分割的块数减一
-        int fileLines = 23669283;//输入文件的行数
+        int splitNum = 4-1;//要分割的块数减一
+        int fileLines = 23;//输入文件的行数
         long perSplitLines = fileLines / splitNum;//每个块的行数
         for (int i = 0; i <= splitNum; ++i)
         {
             //分割
             //每个块建立一个输出
-            FileWriter output = new FileWriter("/home/haoqiong/part" + i + ".txt");
+            FileWriter output = new FileWriter("D://" + i + ".txt");
             String line = null;
             //逐行读取，逐行输出
             for (long lineCounter = 0; lineCounter < perSplitLines && (line = input.readLine()) != null; ++lineCounter)
