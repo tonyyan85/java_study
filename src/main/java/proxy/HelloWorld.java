428476a54e8d5d5865eac0013c718deb21afd686
yan <computeryd@gmail.com>
2016-03-27 21:31:53 +0800

    动态代理代码

---
+++ src/main/java/proxy/HelloWorld.java
@@ -0,0 +1,8 @@
+package proxy;
+
+/**
+ * Created by yan on 2016/3/27.
+ */
+public interface HelloWorld {
+    public void sayHelloWorld();
+}
---
+++ src/main/java/proxy/HelloWorldImpl.java
@@ -0,0 +1,12 @@
+package proxy;
+
+/**
+ * Created by yan on 2016/3/27.
+ */
+public class HelloWorldImpl implements HelloWorld{
+
+    public void sayHelloWorld() {
+        System.out.println("HelloWorld!");
+    }
+
+}
