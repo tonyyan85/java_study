dda7328383eda44e9fabc7a28ffb03eb8cbdc633
鄢迪 <yandi02@meituan.com>
2017-04-15 21:50:51 +0800

    no message

---
+++ src/main/java/list/TestArray.java
@@ -0,0 +1,14 @@
+package list;
+
+/**
+ * Created by yandi on 2017/3/29.
+ */
+public class TestArray {
+    public static void main(String args[]){
+        String[] strs = null;
+        for(String str : strs){
+            System.out.println("fff");
+        }
+        System.out.print("gggggg");
+    }
+}
